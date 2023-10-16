03eb58acba5b4e78dcaa5506d1da07111bef93a7
janidulnkg1 <janidulnkg1@gmail.com>
2023-10-16 14:56:13 +0530

    Add Dependency Injection
    ProductRepository wrapped with IProductRepository

---
+++ Product_api/Product_api/Controllers/ProductController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Product_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+    }
+}
--- Product_api/Product_api/Program.cs
+++ Product_api/Product_api/Program.cs
@@ -1,9 +1,14 @@
+using Product_api.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddTransient<IProductRepository, ProductRepository>();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
